8b80f3747a9ae3deb5658ca3be6079b64ddf4ea7
Julian_A_Wardana <julian.wardana@binus.ac.id>
2022-08-29 21:37:59 +0700

    feat: add state and button styled components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
+import React, { useState } from 'react';
+import styled from "styled-components";
 import logo from './logo.svg';
 import './App.css';
 
+const BtnIncrease = styled.button`
+    background: ${props => props.primary ? "#59CE8F" : "white"};
+    color: ${props => props.primary ? "white" : "#59CE8F"};
+
+    cursor: pointer;
+    font-size: 1em;
+    margin: 1em;
+    padding: 0.25em 1em;
+    border: 2px solid #59CE8F;
+    border-radius: 3px;
+`;
+
+const BtnDecrease = styled.button`
+    background: ${props => props.primary ? "#FF1E00" : "white"};
+    color: ${props => props.primary ? "white" : "#FF1E00"};
+
+    cursor: pointer;
+    font-size: 1em;
+    margin: 1em;
+    padding: 0.25em 1em;
+    border: 2px solid #FF1E00;
+    border-radius: 3px;
+`;
+
 function App() {
-    const name = 'Julian Alifirman Wardana';
+    const [count, setCount] = useState(0);
+    const [name] = useState('Julian Alifirman Wardana');
+
     return (
         <div className="App">
         <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <p>
-            Hello my name is {name}
+                Hello my name is {name}
             </p>
+            <p>
+                {count}
+            </p>
+            <div>
+                <BtnDecrease onClick={() => setCount(count - 1)}>Decrease</BtnDecrease>
+                <BtnIncrease onClick={() => setCount(count + 1)}>Incerease</BtnIncrease>
+            </div>
             <a
             className="App-link"
             href="https://reactjs.org"
