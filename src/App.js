a196ab0442a1ab24094d1778c54ded2646753aa0
Julian_A_Wardana <julian.wardana@binus.ac.id>
2022-08-29 22:21:55 +0700

    feat: add style to name and button

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
 import React, { useState } from 'react';
-import styled from "styled-components";
+// import styled from "styled-components";
 import logo from './logo.svg';
+import styles from './assets/text.module.css';
 import './App.css';
+import './assets/style.css';
 
-const BtnIncrease = styled.button`
-    background: ${props => props.primary ? "#59CE8F" : "white"};
-    color: ${props => props.primary ? "white" : "#59CE8F"};
+// const BtnIncrease = styled.button`
+//     background: ${props => props.primary ? "#59CE8F" : "white"};
+//     color: ${props => props.primary ? "white" : "#59CE8F"};
 
-    cursor: pointer;
-    font-size: 1em;
-    margin: 1em;
-    padding: 0.25em 1em;
-    border: 2px solid #59CE8F;
-    border-radius: 3px;
-`;
+//     cursor: pointer;
+//     font-size: 1em;
+//     margin: 1em;
+//     padding: 0.25em 1em;
+//     border: 2px solid #59CE8F;
+//     border-radius: 3px;
+// `;
 
-const BtnDecrease = styled.button`
-    background: ${props => props.primary ? "#FF1E00" : "white"};
-    color: ${props => props.primary ? "white" : "#FF1E00"};
+// const BtnDecrease = styled.button`
+//     background: ${props => props.primary ? "#FF1E00" : "white"};
+//     color: ${props => props.primary ? "white" : "#FF1E00"};
 
-    cursor: pointer;
-    font-size: 1em;
-    margin: 1em;
-    padding: 0.25em 1em;
-    border: 2px solid #FF1E00;
-    border-radius: 3px;
-`;
+//     cursor: pointer;
+//     font-size: 1em;
+//     margin: 1em;
+//     padding: 0.25em 1em;
+//     border: 2px solid #FF1E00;
+//     border-radius: 3px;
+// `;
 
 function App() {
     const [count, setCount] = useState(0);
@@ -35,15 +37,15 @@ function App() {
         <div className="App">
         <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <p>
+            <p className={styles.name}>
                 Hello my name is {name}
             </p>
             <p>
                 {count}
             </p>
             <div>
-                <BtnDecrease onClick={() => setCount(count - 1)}>Decrease</BtnDecrease>
-                <BtnIncrease onClick={() => setCount(count + 1)}>Incerease</BtnIncrease>
+                <button onClick={() => setCount(count - 1)}>Decrease</button>
+                <button onClick={() => setCount(count + 1)}>Incerease</button>
             </div>
             <a
             className="App-link"
